05d44e6a8a5a2fc890097dfbb3c1a7cbda60557d
Quaker762 <Jooster669@gmail.com>
2014-12-23 13:38:26 +1100

    Working GL test

--- src/game/CTitleState.cpp
+++ src/game/CTitleState.cpp
@@ -28,6 +28,8 @@ void CTitleState::update()
 void CTitleState::render()
 {
    // SDL_FillRect(window.r_getDrawSurface(), NULL, SDL_MapRGB(window.r_getDrawSurface()->format, 0xFF, 0xFF, 0x00));
+    glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
 }
 
 
