2e3646969c46461c670c2f991e2a7437ee167326
Joccaren <matthewroutley6@gmail.com>
2015-01-06 18:04:57 +1100

    Fucking C -> Convert to C++ PLZ

--- src/TGAReader.cpp
+++ src/TGAReader.cpp
@@ -20,7 +20,7 @@ void TGAReader::tgaLoadHeader(FILE *file)
     fread(&cGarbage, sizeof(unsigned char), 1, file);
 
     //Gets info (And discards garbage), type must be 2 or 3 (Uncompressed RBG(A) or Uncompressed Greyscale respectively)
-    fread(&info->type, sizeof(unsigned char), 1, file);
+    fread(type, sizeof(unsigned char), 1, file);
 
     fread(&iGarbage, sizeof(short int), 1, file);
     fread(&iGarbage, sizeof(short int), 1, file);
@@ -28,9 +28,9 @@ void TGAReader::tgaLoadHeader(FILE *file)
     fread(&iGarbage, sizeof(short int), 1, file);
     fread(&iGarbage, sizeof(short int), 1, file);
 
-    fread(&info->width, sizeof(short int), 1, file);
-    fread(&info->height, sizeof(short int), 1, file);
-    fread(&info->pixDepth, sizeof(unsigned char), 1, file);
+    fread(width, sizeof(short int), 1, file);
+    fread(height, sizeof(short int), 1, file);
+    fread(pixDepth, sizeof(unsigned char), 1, file);
 
     fread(&cGarbage, sizeof(unsigned char), 1, file);
 }
@@ -45,11 +45,11 @@ void tgaLoadImageData(FILE *file)
     unsigned char aux;
 
     //Set mode
-    mode = info->pixeldepth / 8;
+    mode = pixeldepth / 8;
     //Calculate amount of data to be read
-    total = info->height * info->width * mode;
+    total = height * width * mode;
     //Load bits into imageData
-    fread(info->imageData, sizeof(unsigned char), total, file);
+    fread(imageData, sizeof(unsigned char), total, file);
 
     //Convert from BGR(A) to RGB(A) if image is coloured
 
@@ -58,8 +58,8 @@ void tgaLoadImageData(FILE *file)
         for (i=0; i < total; i+=mode)
         {
             aux = info->imageData[i];
-            info->imageData[i] = info->imageData[i+2];
-            info->imageData[i+2] = aux;
+            imageData[i] = info->imageData[i+2];
+            imageData[i+2] = aux;
         }
     }
 }
@@ -68,7 +68,6 @@ void tgaLoadImageData(FILE *file)
 void tgaInfo(char *filename)
 {
     FILE *file;
-    tgaInfo *info;
     int mode, total;
     //Allocate Memory
     info = (tgaInfo *)malloc(sizeof(tgaInfo));
